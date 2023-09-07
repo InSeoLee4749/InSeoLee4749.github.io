522770a5d8f7fc266774da2a3e2ff9ea982733cf
InSeoLee4749 <in474900@gmail.com>
2023-09-07 17:24:04 +0900

    hd keyframes & 반응형처리 / navi 제작 및 fixed 처리 / footer 수정 / quick content 추가

--- js/my.js
+++ js/my.js
@@ -1,28 +1,39 @@
 
 
-window.onload = function(){
-    var swiperAbout = new Swiper(".mySwiper", {
-        slidesPerView: 1,
-        spaceBetween: 20,
-        navigation: {
-            nextEl: ".mySwiper .swiper-button-next",
-            prevEl: ".mySwiper .swiper-button-prev",
-        },
-        breakpoints: {
-          
-            991: {
-              slidesPerView: 2,
-              spaceBetween: 40,
-            }
-          },
+    window.onload = function(){
+        var swiperAbout = new Swiper(".mySwiper", {
+            slidesPerView: 1,
+            spaceBetween: 20,
+            navigation: {
+                nextEl: ".mySwiper .swiper-button-next",
+                prevEl: ".mySwiper .swiper-button-prev",
+            },
+            breakpoints: {
+                991: {
+                slidesPerView: 2,
+                spaceBetween: 40,
+                }
+            },
+        });
+
+        var swiper = new Swiper(".mySwiper2", {
+            slidesPerView: 1,
+            navigation: {
+            nextEl: ".mySwiper2 .swiper-button-next",
+            prevEl: ".mySwiper2 .swiper-button-prev",
+            },
+        });
+    }
+
+    window.addEventListener('scroll', function() {
+        var navi = document.querySelector('.navi'); // navi 요소를 가져옵니다.
+        var scrollY = window.scrollY || window.pageYOffset;
+
+        if (scrollY > 100) { // 스크롤 위치가 100 이상이라면
+            navi.classList.add('fixed-top'); // fixed-top 클래스 추가
+        } else { // 스크롤 위치가 100 미만이라면
+            navi.classList.remove('fixed-top'); // fixed-top 클래스 제거
+        }
     });
 
-    var swiper = new Swiper(".mySwiper2", {
-        slidesPerView: 1,
-        navigation: {
-        nextEl: ".mySwiper2 .swiper-button-next",
-        prevEl: ".mySwiper2 .swiper-button-prev",
-        },
-    });
-}
 
